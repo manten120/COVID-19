37ebabba73a6cfbf2e1dc1fa6ff961c8d11a573f
stacru <hamaw8888@gmail.com>
2020-03-17 23:42:36 +0900

    関数をモジュール化

--- app.js
+++ app.js
@@ -2,43 +2,18 @@
 
 const fs = require('fs');
 const readline = require('readline');
-const rs = fs.createReadStream('./csv/2020-09-zensu.csv');
+const rs = fs.createReadStream('./csv/2020-10-zensu.csv');
 const rl = readline.createInterface({'input': rs, 'output': {}});
 const prompts = require('prompts');
+const functions = require('./functions.js');
 
 const prefectureDataMap = new Map();
 
-function bar() {console.log('--------------------------------------');}
-
-function searchResult(command) {
-  const obj = prefectureDataMap.get(command);
-  for (let key in obj ){
-    console.log(`${key}: ${obj[key]}`);
-  }
-}
-
 rl.on('line', (lineString)=>{
-  const columns = lineString.split(',');
-  const prefecture = columns[0];
-  const infectedPeople = parseInt(columns[29]);
-  const accumulation = parseInt(columns[30]);
-  
-  let value = prefectureDataMap.get(prefecture);
-  if (!value) {
-    value = {
-      今週の感染者数: 0,
-      累積: 0,
-    };
-  }
-  value.今週の感染者数 = infectedPeople;
-  value.累積 = accumulation;
-  prefectureDataMap.set(prefecture, value);
+  functions.ConvertCsvToMap(lineString, prefectureDataMap);
 });
 
 rl.on('close', () => {
-  prefectureDataMap.delete('報告数・累積報告数、疾病・都道府県別');
-  prefectureDataMap.delete('');
-
   (async function () {
     // 入力を待ち受ける内容
     let questions = {
@@ -57,13 +32,13 @@ rl.on('close', () => {
     let command = response.command;
 
     if (command === '総数') {
-      bar();
-      searchResult(command);
-      bar();
+      functions.bar();
+      functions.searchResult(command, prefectureDataMap);
+      functions.bar();
     } else if (command === '全国') {
-      bar();
+      functions.bar();
       console.log(prefectureDataMap);
-      bar();
+      functions.bar();
     } else if (command === 'search') {
       (async function () {
         let question = {
@@ -73,10 +48,10 @@ rl.on('close', () => {
         };
         let response = await prompts(question);
         command = response.command;
-        bar();
+        functions.bar();
         console.log(`${command}の新型コロナ感染状況`);
-        searchResult(command);
-        bar();
+        functions.searchResult(command, prefectureDataMap);
+        functions.bar();
       })();
     }
   })()
